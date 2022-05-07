5240d8600c0efd0adcf5f75cd556cf145f83a559
Aaron Hillegass <gtg129u@gatech.edu>
2022-05-07 13:51:15 -0400

    Added first vectors chapter

---
+++ Modules/TrianglesCircles/first_vectors.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Create two vectors
+v = np.array([2,3,4])
+u = np.array([-1,-2,3])
+print(f"u = {u}, v = {v}")
+
+# Add them
+w = v + u
+print(f"u + v = {w}")
+
+# Multiply by a scalar
+w = v * 3
+print(f"v * 3 = {w}")
