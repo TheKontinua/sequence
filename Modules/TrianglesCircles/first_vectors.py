70dc82859a037dadca562f250e97d01d72c0431e
Aaron Hillegass <gtg129u@gatech.edu>
2022-05-08 14:09:25 -0400

    Added dot product

--- Modules/TrianglesCircles/first_vectors.py
+++ Modules/TrianglesCircles/first_vectors.py
@@ -12,3 +12,17 @@ print(f"u + v = {w}")
 # Multiply by a scalar
 w = v * 3
 print(f"v * 3 = {w}")
+
+# Get the magnitude
+mv = np.linalg.norm(v)
+mu = np.linalg.norm(u)
+print(f"|v| = {mv:.2f}, |u| = {mu:.2f}")
+
+# Take the dot product
+d = v @ u
+print("v @ u =", d)
+
+# Get the angle between the vectors
+a = np.arccos(d / (mv * mu))
+print(f"The angle between u and v is {a * 180 / np.pi:.2f} degrees")
+
