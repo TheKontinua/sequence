f08098620e7f42372011fa0ae1295dfd87ded59c
Aaron Hillegass <gtg129u@gatech.edu>
2023-06-19 16:25:25 -0400

    Moved generated workbooks, allows multiple videos and resources, cleans more

--- Build/gather_resources.py
+++ Build/gather_resources.py
@@ -6,29 +6,19 @@ import util
 import shutil
 from jinja2 import Environment, FileSystemLoader
 
-# Paths are from Intermediate
 vol_count = 36
-
 environment = Environment(loader=FileSystemLoader("Support"))
 template = environment.get_template("resource_template.html")
 
-def usage():
-    print("Usage: python3 gather_resources <i>")
-    print("   or  python3 gather_resources all")
-    sys.exit(1)
-
 if not os.path.exists("Resources"):
     os.makedirs("Resources")
-    shutil.copyfile("Support/kontinua.css", "Resources/kontinua.css")
+shutil.copyfile("Support/kontinua.css", "Resources/kontinua.css")
 
 if not os.path.exists("user.cfg"):
     shutil.copyfile("Support/default.cfg", "user.cfg")
 
 with open("user.cfg", "r") as config_fd:
     config = json.load(config_fd)
-
-if len(sys.argv) < 2:
-    usage()
 
 # Gather all metadatas    
 book_nums = [str(x).zfill(2) for x in range(1, vol_count + 1)]
@@ -40,12 +30,8 @@ for book in book_nums:
     books_metadata.append(book_metadatas)
     all_topics.update(topics)
 
-arg1 = sys.argv[1]
-if arg1 == "all":
-    book_indices = list(range(vol_count))
-else:
-    book_indices = [int(arg1) - 1]
-
+# Actually generate the HTML pages
+book_indices = list(range(vol_count))
 for i in book_indices:
     book_str = str(i + 1).zfill(2)
     metadatas = books_metadata[i]
