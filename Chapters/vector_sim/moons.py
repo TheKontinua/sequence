77d81c3a3b6cdbbec2de4c25012e9f88385fdcd2
Aaron Hillegass <gtg129u@gatech.edu>
2023-11-15 17:55:25 -0500

    Added code for vector_sim

---
+++ Chapters/vector_sim/moons.py
@@ -0,0 +1,170 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.patches import Circle, FancyArrow
+from matplotlib.text import Text
+
+# Constants
+G = 6.67430e-11  # Gravitational constant (Nm^2/kg^2)
+SEC_PER_DAY = 24 * 60 * 60  # How many seconds in a day?
+# MAX_TIME = 100 * SEC_PER_DAY  # 100 days
+MAX_TIME = 400 * SEC_PER_DAY  # 100 days
+TIME_STEP = 12 * 60 * 60  #
+FRAMECOUNT = MAX_TIME / TIME_STEP  # How many frames in animation
+ANI_INTERVAL = 1000 / 50  # ms for each frame in animation
+
+# The velocity and acceleration vectors are invisible
+# unless we scale them up.  A lot.
+VSCALE = 140000.0
+ASCALE = VSCALE * 800000.0
+
+# Create the inital state of Moon 1
+m1 = {
+    "mass": 6.0e22,  # kg
+    "position": np.array([0.0, 200_000_000]),  # m
+    "velocity": np.array([100.0, 25.0]),  # m/s
+    "radius": 1_500_000.0,  # m
+    "color": "red",  # For plotting
+}
+
+# Create the inital state of Moon 2
+m2 = {
+    "mass": 11.0e22,  # kg
+    "position": np.array([0.0, -150_000_000]),  # m
+    "velocity": np.array([-45.0, 2.0]),  # m/s
+    "radius": 2_000_000.0,  # m
+    "color": "blue",  # For plotting
+}
+
+# Calculate the initial position and velocity of the center of mass
+tm = m1["mass"] + m2["mass"]  # Total mass
+cm_position = (m1["mass"] * m1["position"] + m2["mass"] * m2["position"]) / tm
+cm_velocity = (m1["mass"] * m1["velocity"] + m2["mass"] * m2["velocity"]) / tm
+
+# Start at time zero seconds
+current_time = 0.0
+
+# Create the figure and axis
+fig, ax = plt.subplots(1, figsize=(7.2, 10))
+
+# Set up the axes
+ax.set_xlabel("x (m)")
+ax.set_xlim((-1.2e8, 4e8))
+ax.set_ylabel("y (m)")
+ax.set_ylim((-1.6e8, 5.5e8))
+ax.set_aspect("equal", adjustable="box")
+fig.tight_layout()
+
+# Create artists that will be edited in animation
+time_text = ax.add_artist(Text(0.03, 0.95, "", transform=ax.transAxes))
+circle1 = ax.add_artist(Circle((0, 0), radius=m1["radius"], color=m1["color"]))
+circle2 = ax.add_artist(Circle((0, 0), radius=m2["radius"], color=m2["color"]))
+circle_cm = ax.add_artist(Circle((0, 0), radius=m2["radius"], color="purple"))
+varrow1 = ax.add_artist(FancyArrow(0, 0, 0, 0, color="green", head_width=m1["radius"]))
+varrow2 = ax.add_artist(FancyArrow(0, 0, 0, 0, color="green", head_width=m2["radius"]))
+acc_arrow1 = ax.add_artist(
+    FancyArrow(0, 0, 0, 0, color="purple", head_width=m1["radius"])
+)
+acc_arrow2 = ax.add_artist(
+    FancyArrow(0, 0, 0, 0, color="purple", head_width=m2["radius"])
+)
+
+
+def animate(frame):
+    # Stuff I need in scope from the model
+    global cm_position, cm_velocity, current_time, m1, m2
+
+    # Artists that will be edited
+    global time_text, varrow1, varrow2, acc_arrow1, acc_arrow2, circle1, circle2, circle_cm
+
+    print(f"Updating artists for day {current_time/SEC_PER_DAY:.1f}.")
+
+
+    # Update the positions based on the current velocities
+    m1["position"] = m1["position"] + m1["velocity"] * TIME_STEP
+    m2["position"] = m2["position"] + m2["velocity"] * TIME_STEP
+
+    # Update day label
+    time_text.set_text(f"Day {current_time/SEC_PER_DAY:.0f}")
+
+    # Update positions of circles
+    circle1.set_center(m1["position"])
+    circle2.set_center(m2["position"])
+
+    # Update velocity arrows
+    varrow1.set_data(
+        x=m1["position"][0],
+        y=m1["position"][1],
+        dx=VSCALE * m1["velocity"][0],
+        dy=VSCALE * m1["velocity"][1],
+    )
+    varrow2.set_data(
+        x=m2["position"][0],
+        y=m2["position"][1],
+        dx=VSCALE * m2["velocity"][0],
+        dy=VSCALE * m2["velocity"][1],
+    )
+
+    # print(f"\tMoon 1:({m1['position'][0]:,.1f},{m1['position'][1]:,.1f})")
+    # print(f"\tMoon 2:({m2['position'][0]:,.1f},{m2['position'][1]:,.1f})")
+
+    # Update the center of mass
+    cm_position = cm_position + cm_velocity * TIME_STEP
+    circle_cm.set_center(cm_position)
+
+    # Find the vector from moon1 to moon2
+    delta = m2["position"] - m1["position"]
+
+    # What is the distance between the moons?
+    distance = np.linalg.norm(delta)
+
+    # Have the moons collided?
+    if distance < m1["radius"] + m2["radius"]:
+        print(f"*** Collided {current_time:.1f} seconds in!")
+
+    # What is a unit vector that points from moon1 toward moon2?
+    direction = delta / distance
+
+    # Calculate the magnitude of the gravitational attraction
+    magnitude = G * m1["mass"] * m2["mass"] / (distance**2)
+
+    # Acceleration vector of moons (a = f/m)
+    acceleration1 = direction * magnitude / m1["mass"]
+    acceleration2 = (-1 * direction) * magnitude / m2["mass"]
+
+    # Update the acceleration arrows
+    acc_arrow1.set_data(
+        x=m1["position"][0],
+        y=m1["position"][1],
+        dx=ASCALE * acceleration1[0],
+        dy=ASCALE * acceleration1[1],
+    )
+    acc_arrow2.set_data(
+        x=m2["position"][0],
+        y=m2["position"][1],
+        dx=ASCALE * acceleration2[0],
+        dy=ASCALE * acceleration2[1],
+    )
+
+    # Update the velocity vectors
+    m1["velocity"] = m1["velocity"] + acceleration1 * TIME_STEP
+    m2["velocity"] = m2["velocity"] + acceleration2 * TIME_STEP
+
+    # Update the clock
+    current_time += TIME_STEP
+
+    # Return the artists that need to be redrawn
+    return (
+        time_text,
+        varrow1,
+        varrow2,
+        acc_arrow1,
+        acc_arrow2,
+        circle1,
+        circle2,
+        circle_cm,
+    )
+
+
+animation = FuncAnimation(fig, animate, np.arange(FRAMECOUNT), interval=ANI_INTERVAL)
+animation.save("moons.mp4")
