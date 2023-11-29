de91c97191aeedcb71a0902cc1450fe708056461
Aaron Hillegass <gtg129u@gatech.edu>
2023-11-28 20:48:04 -0500

    Changed script to generate SQL for textbook structure.

--- Build/structure_sql.py
+++ Build/structure_sql.py
@@ -16,27 +16,29 @@ with open("user.cfg", "r") as config_fd:
     config = json.load(config_fd)
 
 # Open the file
-filename = f"globallist.txt"
+filename = f"sql.txt"
 fout = open(filename, "w")
 
-# Gather all metadatas    
-book_nums = [str(x).zfill(2) for x in range(1, vol_count + 1)]
-
 # What languages are we trying to localize for?
-lang_list = ["en_US"]
+lang_list = [["en","US"]]
 
 # Walk the books
-for book in book_nums:
+for x in range(1, vol_count + 1):
+    book = str(x).zfill(2)
     print(f"Gathering titles for book {book}...")
-    print(book, file=fout)
+    print(f"INSERT INTO mentapp_volume (volume_id) VALUES ({x});", file=fout)
     (ids, dirs) = util.dir_list_for_book("../Chapters", book, config["Languages"])
+    order_float = 10.0
     for id in ids:
-        print(f"\t{id}", file=fout)
+        print(f"INSERT INTO mentapp_chapter (chapter_id, ordering, volume_id) VALUES ('{id}', {order_float:.1f}, {x});", file=fout)
+        order_float += 10.0
         for lang in lang_list:
-            loc_path = f"../Chapters/{id}/{lang}"
+            iso_code = f"{lang[0]}_{lang[1]}"
+            loc_path = f"../Chapters/{id}/{iso_code}"
             if os.path.exists(loc_path):
                 title = util.title_for_dir(loc_path)
-                print(f"\t\t{lang}:{title}", file=fout)
+                title = title.replace("'","\'")
+                print(f"INSERT INTO mentapp_chapter_loc (lang_code, dialect_code, title, chapter_id) VALUES ('{lang[0]}', '{lang[1]}', '{title}', '{id}');", file=fout)
 
 fout.close()
 print(f"Complete: {filename}")
