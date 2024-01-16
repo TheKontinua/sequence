17cd8a2f62682b9ab6df8446c4b918b26b483138
Bunny Laden <bunnyladen@mailbox.org>
2024-01-15 16:33:42 -0800

    Incorporated many changes suggested by Aaron. Corrected typos. Working to completely review Workbook 19, including changing the order of the chapters.

--- Chapters/systems_matrices/en_US/matrices_creation.py
+++ Chapters/systems_matrices/en_US/matrices_creation.py
@@ -2,22 +2,22 @@
 import numpy as np
 // Use the function np.array to define a matrix that 
 // contains specific values that you supply.
-A = np.array([[ 5, 1 ,3], 
-              [ 1, -1 ,8], 
-              [ 6, 2 ,1]])
+A = np.array([[5, 1, 3], 
+              [1, -1, 8], 
+              [6, 2, 1]])
 // The transpose function returns 
 A.transpose()
 
 // create a matrix, B
-B = np.array([[ 5, 1 ,6], 
-              [ 1, -1 ,2], 
-              [ 6, 2 ,1]])
+B = np.array([[5, 1, 6], 
+              [1, -1, 2], 
+              [6, 2, 1]])
 B.transpose()
 
 // create a matrix, B
-J = np.array([[ 5, 1 ,3, 0], 
-              [ 1, -1 ,8, 11], 
-              [ 6, 2 ,1,-7]])
+J = np.array([[5, 1, 3, 0], 
+              [1, -1, 8, 11], 
+              [6, 2, 1, -7]])
 J.transpose()
 
 // create an 8 by 10 Zero matrix.
